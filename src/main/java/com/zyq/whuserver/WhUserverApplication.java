7af4eb9439b59164c0e4c659da33a6bb01c8b0f1
cityghost <1136150740@qq.com>
2021-12-07 23:40:36 +0800

    初始化项目

---
+++ src/main/java/com/zyq/whuserver/WhUserverApplication.java
@@ -0,0 +1,13 @@
+package com.zyq.whuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhUserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhUserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyq/whuserver/WhUserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyq.whuserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhUserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
